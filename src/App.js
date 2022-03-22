e80d041e09880e03606d2d6b624492c65466827e
MikeB1234 <mbacha2345@gmail.com>
2022-03-22 14:13:31 -0400

    searching with filtered arrays

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import { BiCalendar } from "react-icons/bi"
 import Search from "./components/Search"
 import AddAppointment from "./components/AddAppointment"
 import AppointmentInfo from "./components/AppointmentInfo"
-
 function App() {
 
   let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  )
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -15,19 +25,19 @@ function App() {
         setAppointmentList(data)
       });
   }, [])
-
   useEffect(() => {
     fetchData()
   }, [fetchData]);
-
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
       <AddAppointment />
-      <Search />
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)} />
+
       <ul className="divide-y divide-gray-200">
-        {appointmentList
+        {filteredAppointments
           .map(appointment => (
             <AppointmentInfo key={appointment.id}
               appointment={appointment}

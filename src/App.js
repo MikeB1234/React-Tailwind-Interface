590471344cc6b2391e92ff73a0c6e37751190d04
MikeB1234 <mbacha2345@gmail.com>
2022-03-22 12:58:12 -0400

    install Tailwind & react-icons, create Search component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BiArchive } from 'react-icons/bi'
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Interface</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className='text-5xl'>
+        <BiArchive className='inline-block text-red-400 mr-3 ' />
+        React Interface
+      </h1>
+      <Search />
     </div>
   );
 }
